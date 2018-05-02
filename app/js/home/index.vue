a4ec401fd24e786496e3664f0f8db959e25ec8d2
lijq123 <lijq123@foxmail.com>
2018-05-03 07:01:43 +0800

    理财精选组件

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -4,6 +4,7 @@
       <Slider></Slider>
       <Novice></Novice>
       <Borrow></Borrow>
+      <Money></Money>
     </div>
 </template>
 
@@ -12,12 +13,14 @@ import Heador from "../public/header.vue";
 import Slider from "./hslider.vue";
 import Novice from "./novice.vue";
 import Borrow from "./borrow.vue";
+import Money from "./money.vue";
 export default {
   components: {
     Heador,
     Slider,
     Novice,
-    Borrow
+    Borrow,
+    Money,
   }
 };
 </script>
