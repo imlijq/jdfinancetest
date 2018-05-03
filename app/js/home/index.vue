fbfc897b95394b096fdb57f2de568f50529724a2
lijq123 <lijq123@foxmail.com>
2018-05-03 21:48:56 +0800

    新品推荐组件

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -5,6 +5,7 @@
       <Novice></Novice>
       <Borrow></Borrow>
       <Money></Money>
+      <Product></Product>
     </div>
 </template>
 
@@ -14,6 +15,7 @@ import Slider from "./hslider.vue";
 import Novice from "./novice.vue";
 import Borrow from "./borrow.vue";
 import Money from "./money.vue";
+import Product from "./product.vue";
 export default {
   components: {
     Heador,
@@ -21,6 +23,7 @@ export default {
     Novice,
     Borrow,
     Money,
+    Product,
   }
 };
 </script>
