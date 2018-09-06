f39d7c71e2f5b712773f4d121614c9939e447eb0
imlijq <imlijq@gmail.com>
2018-09-06 23:22:17 +0800

    删掉无效

--- app/js/ious/islider.vue
+++ app/js/ious/islider.vue
@@ -18,17 +18,17 @@ export default {
     return {
       items: [
         {
-          href: "page0",
+          href: "home",
           src:
             "//img12.360buyimg.com/jrpmobile/jfs/t16798/322/621991060/49914/ec0d539b/5a991ebcN932c6731.jpg?width=750&height=320"
         },
         {
-          href: "page1",
+          href: "money",
           src:
             "//img12.360buyimg.com/jrpmobile/jfs/t8491/303/504628479/60643/100648de/59a94358N92475fca.jpg?width=750&height=320"
         },
         {
-          href: "page2",
+          href: "ious",
           src:
             "//img12.360buyimg.com/jrpmobile/jfs/t8437/321/1758527686/40952/f66d552f/59bf1f41N50d846f9.jpg?width=750&height=320"
         }
